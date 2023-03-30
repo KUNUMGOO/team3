6ab83ddab48873793a55d8034bc7b07b7454426a
unknown <kunusuperit@gmail.com>
2023-03-30 13:03:22 +0600

    start

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import App from "../../App";
+
+const pages = [
+  { name: "Каталог кроватей", link: "/", id: 1 },
+  { name: "Компания", link: "/company", id: 2 },
+  { name: "Блог", link: "/products", id: 3 },
+  { name: "Контакты", link: "/contacts", id: 4 },
+  { name: "", link: "/*", id: 5 },
+];
+
+export default Navbar;
